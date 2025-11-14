ac7e83ebe13a655754292cad94f22233c57c61e3
jet_dbk <kelarevad@gmail.com>
2025-11-14 22:18:03 +0000

    половина второй части

--- bbc/homework_4/homework_4 (2part).py
+++ bbc/homework_4/homework_4 (2part).py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
-
+#columns: PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 with open('tested.csv') as f:
     titanic = pd.read_csv(f, index_col=0)
+
+
+filter1 = titanic.loc[(titanic['Age'] > 30)
+                      & (titanic['Sex'] == 'male')
+                     & (titanic['Pclass'])]
+filter2 = titanic.loc[((titanic['Age'] < 18)
+                      | (titanic['Sex'] == 'female'))
+                      & (titanic['Survived'])]
